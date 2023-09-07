53ab2a2e96b6c6e524cac8bd97f3428c6a2d3e9f
chukwudi5 <stanleyonwumelu@gmail.com>
2023-09-06 21:41:34 -0400

    Create Application view

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import ApplicationSerializer
+from rest_framework.views import APIView
+from django.http import JsonResponse
 
-# Create your views here.
+class ApplicationView(APIView):
+    def post(self, request):
+        serializer = ApplicationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            return JsonResponse(serializer.errors)
