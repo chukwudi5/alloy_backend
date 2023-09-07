c5b33cb69839ea925ff54cdcd7f381fd7b63f70b
chukwudi5 <stanleyonwumelu@gmail.com>
2023-09-06 22:13:22 -0400

    Add outcome fetch in Application view

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .serializers import ApplicationSerializer
+from .services import get_alloy_summary
 from rest_framework.views import APIView
 from django.http import JsonResponse
 
 class ApplicationView(APIView):
     def post(self, request):
+        request.data['outcome'] = get_alloy_summary(request.data)
         serializer = ApplicationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
