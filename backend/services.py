92a04625b9f16090b90babe138deb5c7e879773a
chukwudi5 <stanleyonwumelu@gmail.com>
2023-09-07 10:58:55 -0400

    Use get allow summary parameter as post argument

--- backend/services.py
+++ backend/services.py
@@ -5,14 +5,13 @@ load_dotenv()
 
 def get_alloy_summary(request):
     api_url = 'https://sandbox.alloy.co/v1/evaluations'
-    data = request
 
     username = os.getenv('WORKFLOW_TOKEN')
     password = os.getenv('WORKFLOW_SECRET')
     auth = (username, password)
     
     try:
-        response = requests.post(api_url, data=data, auth=auth)
+        response = requests.post(api_url, data=request, auth=auth)
         if response.status_code == 201:
             result = response.json
     except requests.exceptions.RequestException as e:
